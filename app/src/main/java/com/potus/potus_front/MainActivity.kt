04cd912c63c2e98fcd1884273d8527e7ead264dd
march <marc.heras@estudiantat.upc.edu>
2022-10-09 20:18:17 +0200

    First attempt of a Background

--- app/src/main/java/com/potus/potus_front/MainActivity.kt
+++ app/src/main/java/com/potus/potus_front/MainActivity.kt
@@ -3,7 +3,12 @@ package com.potus.potus_front
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.DropdownMenu
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,7 +16,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.potus.potus_front.ui.theme.BraveGreen
 import com.potus.potus_front.ui.theme.Potus_frontTheme
+import com.potus.potus_front.ui.theme.SoothingGreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +30,44 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     // color = MaterialTheme.colors.background
-                    color = Color(0xB0DD7F)
+                    color = MaterialTheme.colors.background
 
                 ) {
-                    Greeting("Android")
+                    BaseApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    Potus_frontTheme {
-        Greeting("Android")
+fun BaseApp() {
+    Column(Modifier.background(color = SoothingGreen)){
+        TopBar()
+        Surface(color = SoothingGreen, modifier = Modifier.weight(1f)) {
+
+            // Potus in the middle
+
+        }
+        BottomBar()
     }
+}
+
+@Composable
+fun TopBar() {
+    Surface(color = BraveGreen, modifier = Modifier
+        .fillMaxWidth()
+        .height(64.dp)) {
+        DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+            
+        }
+    }
+}
+
+@Composable
+fun BottomBar() {
+    Surface(color = BraveGreen, modifier = Modifier
+        .fillMaxWidth()
+        .height(128.dp)) {}
 }
