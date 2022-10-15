44edc3a4d3f66b11e8a4261cbecbd0d3ec71695e
BertaFito <berta.fito@estudiantat.upc.edu>
2022-10-15 13:41:27 +0200

    Configuració bàsica de l'acció Prune de la tasca #103

---
+++ app/src/main/java/com/potus/potus_front/PruneActivity.kt
@@ -0,0 +1,11 @@
+package com.potus.potus_front
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PruneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plant)
+    }
+}
