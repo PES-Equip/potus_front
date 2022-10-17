f0f8cddedbcbaba57e09bb562eb7808415981abc
BertaFito <berta.fito@estudiantat.upc.edu>
2022-10-17 17:28:54 +0200

    Watering and pruning

--- app/src/main/java/com/potus/potus_front/MainActivity.kt
+++ app/src/main/java/com/potus/potus_front/MainActivity.kt
@@ -5,14 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
@@ -48,12 +49,26 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun BaseApp() {
+    var waterLevelState by remember {mutableStateOf(0)}
+    var collection by remember { mutableStateOf(0)}
+
     Column(Modifier.background(color = SoothingGreen)){
-        TopBar()
+        TopBar(waterLevel = waterLevelState, collection = collection)
         Surface(color = SoothingGreen, modifier = Modifier.weight(1f)) {
             CenterArea()
         }
-        BottomBar()
+        BottomBar(
+            waterLevel = waterLevelState,
+            updateWaterLevel = {
+                    newWaterLevel ->
+                waterLevelState = newWaterLevel
+            },
+            leaves = collection,
+            updateLeaveRecollection = {
+                    collectedLeaves ->
+                collection = collectedLeaves
+            }
+        )
     }
 }
 
@@ -64,70 +79,70 @@ fun CenterArea() {
         Spacer(modifier = Modifier.height(128.dp))
         Box(modifier = Modifier
             .align(CenterHorizontally))
-            {
-                Image(
-                    painter = painterResource(id = R.drawable.test_basic),
-                    "",
-                    modifier = Modifier
-                        .size(360.dp)
-                        .align(Center)
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.planta_basic_tiges),
-                    "",
-                    modifier = Modifier
-                        .size(360.dp)
-                        .align(Center)
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.planta_basic_fulles),
-                    "",
-                    modifier = Modifier
-                        .size(360.dp)
-                        .align(Center)
-                )
-            }
+        {
+            Image(
+                painter = painterResource(id = R.drawable.test_basic),
+                "",
+                modifier = Modifier
+                    .size(360.dp)
+                    .align(Center)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.planta_basic_tiges),
+                "",
+                modifier = Modifier
+                    .size(360.dp)
+                    .align(Center)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.planta_basic_fulles),
+                "",
+                modifier = Modifier
+                    .size(360.dp)
+                    .align(Center)
+            )
+        }
     }
 
 }
 
 // En el composable Top Bar caldria descobrir com separar els composables de water i leaves.
 @Composable
-fun TopBar() {
-        Row(
-            Modifier
-                .background(color = BraveGreen)
-                .fillMaxWidth()
-                .height(64.dp)) {
-            //DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {}
-            Spacer(modifier = Modifier.weight(1f))
-            Box(modifier = Modifier
-                .align(CenterVertically)
-                .width(64.dp)
-                .height(30.dp)
-                .clip(RoundedCornerShape(15.dp))
-                .background(color = Color(0x0CFFFFFF))){
-                Text(modifier = Modifier
-                    .align(Center),
-                    text = "water")
-            }
-            Spacer(modifier = Modifier.weight(0.02f))
-            Box(modifier = Modifier
-                .align(CenterVertically)
-                .width(64.dp)
-                .height(30.dp)
-                .clip(RoundedCornerShape(15.dp))
-                .background(color = Color(0x0CFFFFFF))){
-                Text(modifier = Modifier
-                    .align(Center),
-                    text = "leaves")
-            }
-            Spacer(modifier = Modifier.weight(0.075f))
+fun TopBar(waterLevel: Int, collection: Int) {
+    Row(
+        Modifier
+            .background(color = BraveGreen)
+            .fillMaxWidth()
+            .height(64.dp)) {
+        Spacer(modifier = Modifier.weight(1f))
+        Box(modifier = Modifier
+            .align(CenterVertically)
+            .width(64.dp)
+            .height(30.dp)
+            .clip(RoundedCornerShape(15.dp))
+            .background(color = Color(0x0CFFFFFF))){
+            Text(modifier = Modifier
+                .align(Center),
+                text = "$waterLevel %" )
         }
+        Spacer(modifier = Modifier.weight(0.02f))
+        Box(modifier = Modifier
+            .align(CenterVertically)
+            .width(64.dp)
+            .height(30.dp)
+            .clip(RoundedCornerShape(15.dp))
+            .background(color = Color(0x0CFFFFFF))){
+            Text(modifier = Modifier
+                .align(Center),
+                text = "$collection")
+        }
+        Spacer(modifier = Modifier.weight(0.075f))
+    }
 }
 
 @Composable
-fun BottomBar() {
+fun BottomBar(waterLevel: Int, updateWaterLevel: (Int) -> Unit,
+              leaves:Int, updateLeaveRecollection:(Int) -> Unit) {
     val heightBottomBar = 192.dp
     val heightCircle = 125.dp
     val heightTotal = heightBottomBar+heightCircle/2
@@ -161,6 +176,7 @@ fun BottomBar() {
                         painter = painterResource(id = R.drawable.icona_podar),
                         "",
                         modifier = Modifier
+                            .clickable(onClick = { updateLeaveRecollection(leaves + 1) })
                             .padding(8.dp)
                             .size(heightButton)
                             .align(CenterVertically))
@@ -178,6 +194,7 @@ fun BottomBar() {
                         painter = painterResource(id = R.drawable.icona_regadora),
                         "",
                         modifier = Modifier
+                            .clickable(onClick = { updateWaterLevel(waterLevel + 10) })
                             .size(heightButton)
                             .align(CenterVertically))
                 }
@@ -186,7 +203,7 @@ fun BottomBar() {
         Surface(
             color = BraveGreen,
             modifier = Modifier
-                .padding(start = 10.dp, end = 10.dp, bottom = (heightBottomBar - heightCircle/2))
+                .padding(start = 10.dp, end = 10.dp, bottom = (heightBottomBar - heightCircle / 2))
                 .align(TopCenter)
                 .size(heightCircle)
                 .clip(CircleShape)
@@ -203,3 +220,35 @@ fun BottomBar() {
         }
     }
 }
+
+
+//fun PopupDragDemo() {
+//    val offset = remember { mutableStateOf(Offset.Zero) }
+//
+//    Column {
+//        Text("That is a pop up with a dragGestureFilter on it.  You can drag it around!")
+//        BoxWithConstraints(
+//            alignment = Alignment.TopStart,
+//            offset = offset.value.round()
+//        ) {
+//            Box {
+//                Box(
+//                    Modifier
+//                        .pointerInput(Unit) {
+//                            detectDragGestures { _, dragAmount ->
+//                                offset.value = offset.value + dragAmount
+//                            }
+//                        }
+//                        .size(70.dp)
+//                        .background(Color.Green, CircleShape),
+//                    contentAlignment = Alignment.Center
+//                ) {
+//                    Text(
+//                        text = "This is a popup!",
+//                        textAlign = TextAlign.Center
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}
