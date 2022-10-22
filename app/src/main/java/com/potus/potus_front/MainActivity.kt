665cb7870fa659d7506835db7c9f0f55c10a3ac3
march <marc.heras@estudiantat.upc.edu>
2022-10-22 18:42:22 +0200

    Main window update: Separated the Composables out of the MainActivity.kt file and into composables.KtComposableParts.kt

--- app/src/main/java/com/potus/potus_front/MainActivity.kt
+++ app/src/main/java/com/potus/potus_front/MainActivity.kt
@@ -6,27 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.material.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment.Companion.BottomCenter
-import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Alignment.Companion.CenterVertically
-import androidx.compose.ui.Alignment.Companion.TopCenter
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.potus.potus_front.ui.theme.BraveGreen
+import com.potus.potus_front.composables.BottomBar
+import com.potus.potus_front.composables.CenterArea
+import com.potus.potus_front.composables.TopBar
 import com.potus.potus_front.ui.theme.Potus_frontTheme
 import com.potus.potus_front.ui.theme.SoothingGreen
 import com.potus.potus_front.controllers.PotusController
-//import com.potus.potus_front.composables.[composable]
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,161 +58,6 @@ fun BaseApp() {
                 collection = collectedLeaves
             }
         )
-    }
-}
-
-
-@Composable
-fun CenterArea() {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Spacer(modifier = Modifier.height(128.dp))
-        Box(modifier = Modifier
-            .align(CenterHorizontally))
-        {
-            Image(
-                painter = painterResource(id = R.drawable.test_basic),
-                "",
-                modifier = Modifier
-                    .size(360.dp)
-                    .align(Center)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.planta_basic_tiges),
-                "",
-                modifier = Modifier
-                    .size(360.dp)
-                    .align(Center)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.planta_basic_fulles),
-                "",
-                modifier = Modifier
-                    .size(360.dp)
-                    .align(Center)
-            )
-        }
-    }
-
-}
-
-// En el composable Top Bar caldria descobrir com separar els composables de water i leaves.
-@Composable
-fun TopBar(waterLevel: Int, collection: Int) {
-    Row(
-        Modifier
-            .background(color = BraveGreen)
-            .fillMaxWidth()
-            .height(64.dp)) {
-        Spacer(modifier = Modifier.weight(1f))
-        Box(modifier = Modifier
-            .align(CenterVertically)
-            .width(64.dp)
-            .height(30.dp)
-            .clip(RoundedCornerShape(15.dp))
-            .background(color = Color(0x0CFFFFFF))){
-            Text(modifier = Modifier
-                .align(Center),
-                text = "$waterLevel %" )
-        }
-        Spacer(modifier = Modifier.weight(0.02f))
-        Box(modifier = Modifier
-            .align(CenterVertically)
-            .width(64.dp)
-            .height(30.dp)
-            .clip(RoundedCornerShape(15.dp))
-            .background(color = Color(0x0CFFFFFF))){
-            Text(modifier = Modifier
-                .align(Center),
-                text = "$collection")
-        }
-        Spacer(modifier = Modifier.weight(0.075f))
-    }
-}
-
-@Composable
-fun BottomBar(waterLevel: Int, updateWaterLevel: (Int) -> Unit,
-              leaves:Int, updateLeaveRecollection:(Int) -> Unit) {
-    val heightBottomBar = 192.dp
-    val heightCircle = 125.dp
-    val heightTotal = heightBottomBar+heightCircle/2
-    val heightButton = 125.dp
-    val widthButton = 140.dp
-    Box(modifier = Modifier
-        .fillMaxWidth()
-        .height(heightTotal)) {
-        BoxWithConstraints(
-            modifier = Modifier
-                .align(BottomCenter)
-                .fillMaxWidth()
-                .height(heightBottomBar)
-                .background(BraveGreen)
-        )
-        {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(heightTotal)
-            ) {
-                Surface(
-                    color = SoothingGreen,
-                    modifier = Modifier
-                        .align(CenterVertically)
-                        .width(widthButton)
-                        .height(heightButton)
-                        .clip(RoundedCornerShape(10.dp))
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.icona_podar),
-                        "",
-                        modifier = Modifier
-                            .clickable(onClick = {
-                                updateLeaveRecollection(leaves + 1)
-                                PotusController.updateLeaves(leaves + 1)
-                            })
-                            .padding(8.dp)
-                            .size(heightButton)
-                            .align(CenterVertically))
-                }
-                Spacer(modifier = Modifier.weight(1f))
-                Surface(
-                    color = SoothingGreen,
-                    modifier = Modifier
-                        .align(CenterVertically)
-                        .width(widthButton)
-                        .height(heightButton)
-                        .clip(RoundedCornerShape(10.dp))
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.icona_regadora),
-                        "",
-                        modifier = Modifier
-                            .clickable(onClick = {
-                                updateWaterLevel(waterLevel + 10)
-                                PotusController.updateWater(waterLevel + 10)
-                            })
-                            .size(heightButton)
-                            .align(CenterVertically))
-                }
-            }
-        }
-        Surface(
-            color = BraveGreen,
-            modifier = Modifier
-                .padding(start = 10.dp, end = 10.dp, bottom = (heightBottomBar - heightCircle / 2))
-                .align(TopCenter)
-                .size(heightCircle)
-                .clip(CircleShape)
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.icona_jardi),
-                "",
-                modifier = Modifier
-                    .padding(8.dp)
-                    .size(heightCircle)
-                    .align(Center)
-                    .clip(CircleShape)
-                    .background(color = SoothingGreen))
-        }
     }
 }
 
--- app/src/main/java/com/potus/potus_front/composables/KtComposableParts.kt
+++ app/src/main/java/com/potus/potus_front/composables/KtComposableParts.kt
@@ -1,2 +1,176 @@
 package com.potus.potus_front.composables
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.potus.potus_front.R
+import com.potus.potus_front.controllers.PotusController
+import com.potus.potus_front.ui.theme.BraveGreen
+import com.potus.potus_front.ui.theme.SoothingGreen
+
+// En el composable Top Bar caldria descobrir com separar els composables de water i leaves.
+@Composable
+fun TopBar(waterLevel: Int, collection: Int) {
+    Row(
+        Modifier
+            .background(color = BraveGreen)
+            .fillMaxWidth()
+            .height(64.dp)) {
+        Spacer(modifier = Modifier.weight(1f))
+        Box(modifier = Modifier
+            .align(Alignment.CenterVertically)
+            .width(64.dp)
+            .height(30.dp)
+            .clip(RoundedCornerShape(15.dp))
+            .background(color = Color(0x0CFFFFFF))){
+            Text(modifier = Modifier
+                .align(Alignment.Center),
+                text = "$waterLevel %" )
+        }
+        Spacer(modifier = Modifier.weight(0.02f))
+        Box(modifier = Modifier
+            .align(Alignment.CenterVertically)
+            .width(64.dp)
+            .height(30.dp)
+            .clip(RoundedCornerShape(15.dp))
+            .background(color = Color(0x0CFFFFFF))){
+            Text(modifier = Modifier
+                .align(Alignment.Center),
+                text = "$collection")
+        }
+        Spacer(modifier = Modifier.weight(0.075f))
+    }
+}
+
+
+@Composable
+fun CenterArea() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Spacer(modifier = Modifier.height(128.dp))
+        Box(modifier = Modifier
+            .align(Alignment.CenterHorizontally))
+        {
+            Image(
+                painter = painterResource(id = R.drawable.test_basic),
+                "",
+                modifier = Modifier
+                    .size(360.dp)
+                    .align(Alignment.Center)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.planta_basic_tiges),
+                "",
+                modifier = Modifier
+                    .size(360.dp)
+                    .align(Alignment.Center)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.planta_basic_fulles),
+                "",
+                modifier = Modifier
+                    .size(360.dp)
+                    .align(Alignment.Center)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun BottomBar(waterLevel: Int, updateWaterLevel: (Int) -> Unit,
+              leaves:Int, updateLeaveRecollection:(Int) -> Unit) {
+    val heightBottomBar = 192.dp
+    val heightCircle = 125.dp
+    val heightTotal = heightBottomBar+heightCircle/2
+    val heightButton = 125.dp
+    val widthButton = 140.dp
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(heightTotal)) {
+        BoxWithConstraints(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .height(heightBottomBar)
+                .background(BraveGreen)
+        )
+        {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(heightTotal)
+            ) {
+                Surface(
+                    color = SoothingGreen,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .width(widthButton)
+                        .height(heightButton)
+                        .clip(RoundedCornerShape(10.dp))
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.icona_podar),
+                        "",
+                        modifier = Modifier
+                            .clickable(onClick = {
+                                updateLeaveRecollection(leaves + 1)
+                                PotusController.updateLeaves(leaves + 1)
+                            })
+                            .padding(8.dp)
+                            .size(heightButton)
+                            .align(Alignment.CenterVertically))
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                Surface(
+                    color = SoothingGreen,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .width(widthButton)
+                        .height(heightButton)
+                        .clip(RoundedCornerShape(10.dp))
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.icona_regadora),
+                        "",
+                        modifier = Modifier
+                            .clickable(onClick = {
+                                updateWaterLevel(waterLevel + 10)
+                                PotusController.updateWater(waterLevel + 10)
+                            })
+                            .size(heightButton)
+                            .align(Alignment.CenterVertically))
+                }
+            }
+        }
+        Surface(
+            color = BraveGreen,
+            modifier = Modifier
+                .padding(start = 10.dp, end = 10.dp, bottom = (heightBottomBar - heightCircle / 2))
+                .align(Alignment.TopCenter)
+                .size(heightCircle)
+                .clip(CircleShape)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.icona_jardi),
+                "",
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(heightCircle)
+                    .align(Alignment.Center)
+                    .clip(CircleShape)
+                    .background(color = SoothingGreen))
+        }
+    }
+}
