c70afbfe416a2559cc818d6f55e25b5d62f7cd0e
march <marc.heras@estudiantat.upc.edu>
2022-10-22 18:42:11 +0200

    Main window update: Separated the Composables out of the MainActivity.kt file and into composables.KtComposableParts.kt

--- app/src/main/java/com/potus/potus_front/controllers/PotusController.kt
+++ app/src/main/java/com/potus/potus_front/controllers/PotusController.kt
@@ -4,19 +4,23 @@ interface PotusController {
     companion object {
         @JvmStatic
         fun getWater(): Int {
-            return 80
+            // Here a call to the backend service will be performed to get the actual water level.
+            return 0
         }
 
         fun getLeaves(): Int {
+            // Here a call to the backend service will be performed to get the actual leaves count.
             return 0
         }
 
         fun updateWater(updatedWater: Int): Boolean {
-            if(updatedWater in 1..99) return true
+            // Here a call to the backend service will be performed to set the water level.
+            if(updatedWater in 1..100) return true
             return false
         }
 
         fun updateLeaves(updatedLeaves: Int): Boolean {
+            // Here a call to the backend service will be performed to set the leaves count.
             if(updatedLeaves > 0) return true
             return false
         }
