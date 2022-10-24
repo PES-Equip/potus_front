8b3c1631314e90dae1336016b82fe91174abfdc4
BertaFito <berta.fito@estudiantat.upc.edu>
2022-10-24 16:35:51 +0200

    Check working connection Front/Back

--- app/src/main/java/com/potus/potus_front/MainActivity.kt
+++ app/src/main/java/com/potus/potus_front/MainActivity.kt
@@ -6,18 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.potus.potus_front.composables.BottomBar
+import com.potus.potus_front.composables.CenterArea
+import com.potus.potus_front.composables.TopBar
+import com.potus.potus_front.controllers.PotusController
 import com.potus.potus_front.models.TokenState
 import com.potus.potus_front.models.TokenStateViewModel
 import com.potus.potus_front.ui.screens.ApplicationSwitcher
 import com.potus.potus_front.ui.theme.Potus_frontTheme
+import com.potus.potus_front.ui.theme.SoothingGreen
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import timber.log.Timber
 
@@ -55,6 +61,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Preview(showBackground = true)
-@Composable
