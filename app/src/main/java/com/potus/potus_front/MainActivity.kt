45ff407a06043d71bbf5faba884a7b95e8777892
BertaFito <berta.fito@estudiantat.upc.edu>
2022-10-24 16:19:03 +0200

    Check working connection Front/Back

--- app/src/main/java/com/potus/potus_front/MainActivity.kt
+++ app/src/main/java/com/potus/potus_front/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.potus.potus_front.models.TokenState
 import com.potus.potus_front.models.TokenStateViewModel
 import com.potus.potus_front.ui.screens.ApplicationSwitcher
@@ -24,8 +26,6 @@ import timber.log.Timber
 @ExperimentalFoundationApi
 @ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,3 +55,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
