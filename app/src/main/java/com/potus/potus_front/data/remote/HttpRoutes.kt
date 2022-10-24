3946715ca27120287a9fe0841ddf5df535ebf6df
BertaFito <berta.fito@estudiantat.upc.edu>
2022-10-24 16:19:34 +0200

    Check working connection Front/Back

--- app/src/main/java/com/potus/potus_front/data/remote/HttpRoutes.kt
+++
@@ -1,7 +0,0 @@
-package com.potus.potus_front.data.remote
-
-object HttpRoutes {
-
-    private const val BASE_URL = "https://jsonplaceholder.typicode.com"
-    const val POSTS = "$BASE_URL/posts"
-}
