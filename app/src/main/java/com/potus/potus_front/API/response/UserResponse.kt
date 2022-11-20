b35bc47a65434517efcc8a7836e5d55e5a97f8d1
BertaFito <berta.fito@estudiantat.upc.edu>
2022-11-20 21:29:40 +0100

    TG-117 #almost-done Everything is implemented, but the navigation

--- app/src/main/java/com/potus/potus_front/API/response/UserResponse.kt
+++ app/src/main/java/com/potus/potus_front/API/response/UserResponse.kt
@@ -3,6 +3,7 @@ package com.potus.potus_front.API.response
 data class UserResponse(
     var username: String,
     var email: String,
+    var garden_info: NewGardenResponse,
     var currency: Int,
     var potus: PotusResponse,
     var status: String,
--- app/src/main/java/com/potus/potus_front/ui/screens/GardenScreen.kt
+++ app/src/main/java/com/potus/potus_front/ui/screens/GardenScreen.kt
@@ -3,7 +3,6 @@ package com.potus.potus_front.ui.screens
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.selection.toggleable
@@ -15,21 +14,21 @@ import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.potus.potus_front.API.APIService
 import com.potus.potus_front.API.getRetrofit
-import com.potus.potus_front.API.requests.GardenRequest
+import com.potus.potus_front.API.requests.GardenInvitationRequest
 import com.potus.potus_front.R
 import com.potus.potus_front.composables.*
 import com.potus.potus_front.models.TokenState
 import com.potus.potus_front.ui.theme.BraveGreen
 import com.potus.potus_front.ui.theme.Daffodil
-import com.potus.potus_front.ui.theme.RoseRed
 import com.potus.potus_front.ui.theme.SoothingGreen
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -44,8 +43,10 @@ fun GardenScreen() {
     val openDialog = remember { mutableStateOf(false)  }
     val error = remember { mutableStateOf(200)  }
 
-    /*val tokenState = TokenState.current
+    val tokenState = TokenState.current
     val user = tokenState.user!!
+    val members = remember { mutableStateOf(listOf(Pair("THERE ARE NO USERS IN THIS GARDEN", "OWNER"))) }
+    val invitedUser = remember { mutableStateOf(TextFieldValue()) }
 
     LaunchedEffect(Dispatchers.IO) {
         val call = getRetrofit()
@@ -56,24 +57,67 @@ fun GardenScreen() {
             )
 
         if (call.isSuccessful) {
-            //call.body()?.let { tokenState.myGarden(it.garden) }
+            call.body()?.let { members.value = it.members }
         } else {
             //ERROR MESSAGES, IF ANY
             error.value = call.code()
             openDialog.value = true
         }
-    }*/
+    }
 
     Column(Modifier.background(color = Daffodil)) {
         TopBar(
-            waterLevel = 100, //user.potus.waterLevel,
-            collection = 100, //user.currency,
-            username = "JoeBiden", //user.username,
+            waterLevel = user.potus.waterLevel,
+            collection = user.currency,
+            username = user.username,
             addedWater = 0,
             addedLeaves = 0
         )
         Column(modifier = Modifier.weight(1f).background(Daffodil)) {
-            MembersList(listOf(Pair("Me", "OWNER"), Pair("You", "ADMIN"), Pair("He", "ADMIN"), Pair("She", "ADMIN"), Pair("They", "ADMIN")))
+            Spacer(modifier = Modifier.size(8.dp))
+            Row(modifier = Modifier.align(Alignment.Start).padding(8.dp).clip(RoundedCornerShape(10.dp)).background(BraveGreen).fillMaxWidth()) {
+                Image(
+                    painter = painterResource(id = R.drawable.icona_convidar_jardi), "",
+                    modifier = Modifier
+                        .size(64.dp)
+                        .align(CenterVertically)
+                        .padding(start = 8.dp)
+                )
+                OutlinedTextField(
+                    value = invitedUser.value,
+                    onValueChange = { invitedUser.value = it },
+                    label = { Text(text = "Invite User") },
+                    modifier = Modifier
+                        .width(200.dp)
+                        .padding(start = 8.dp, end = 8.dp, bottom = 8.dp)
+                        .align(CenterVertically)
+                )
+                Button(
+                    onClick = {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            val garden = tokenState.user?.garden_info?.garden?.third.toString()
+                            val receiver = invitedUser.value.toString()
+                            val sendGardenInvitation = GardenInvitationRequest(garden = garden, user = receiver)
+                            val call = getRetrofit().create(APIService::class.java)
+                                .sendGardenInvitation(
+                                "Bearer " + tokenState.token,
+                                "gardens/$garden/requests/$receiver",
+                                sendGardenInvitation
+                            )
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(backgroundColor = SoothingGreen),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .align(CenterVertically),
+                    shape = MaterialTheme.shapes.medium
+                ) {
+                    Text(text = "Send!", color = Daffodil)
+                }
+            }
+            //MembersList(listOf(Pair("Me", "OWNER"), Pair("You", "ADMIN"), Pair("He", "ADMIN"), Pair("She", "ADMIN"), Pair("They", "ADMIN")))
+            MembersList(members.value)
         }
         GardenBottomBar(painterResource(id = R.drawable.icona_mercat), painterResource(id = R.drawable.basic), painterResource(id = R.drawable.icona_xat))
     }
@@ -96,7 +140,7 @@ fun MembersList (members: List<Pair<String, String>>) {
 fun MemberItem(member: Pair<String, String>) {
     var gases = arrayOf("C6H6", "Cl2", "CO", "H2S", "HCl", "HCNM", "HCT", "Hg", "NO2", "NO", "NOX", "O3", "PM1", "PM2_5", "PM10", "PS", "SO2")
     var toggled by remember { mutableStateOf(false) }
-    var randomAvatar = Random.nextInt(gases.size)
+    var randomAvatar = gases[Random.nextInt(gases.size)]
 
     Column(
         modifier = Modifier
@@ -108,16 +152,16 @@ fun MemberItem(member: Pair<String, String>) {
             .toggleable(value = toggled, onValueChange = { toggled = it })
             .animateContentSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = CenterHorizontally
     ) {
-        if (!toggled) {
-            Row(modifier = Modifier.align(Alignment.Start)) {
-                Box(
-                    modifier = Modifier
-                        .size(64.dp)
-                        .align(CenterVertically)
-                        .padding(start = 8.dp)
-                ) { CenterArea(gases[randomAvatar]) }
+        Row(modifier = Modifier.align(Alignment.Start)) {
+            Box(
+                modifier = Modifier
+                    .size(64.dp)
+                    .align(CenterVertically)
+                    .padding(start = 8.dp)
+            ) { CenterArea(randomAvatar) }
+            if (!toggled) {
                 Text(
                     text = member.first,
                     fontSize = 20.sp,
@@ -126,21 +170,13 @@ fun MemberItem(member: Pair<String, String>) {
                     modifier = Modifier.padding(start = 8.dp).align(CenterVertically)
                 )
             }
-        }
-        else {
-            Column() {
-                Row(modifier = Modifier.align(Alignment.Start)) {
-                    Box(
-                        modifier = Modifier
-                            .size(64.dp)
-                            .align(CenterVertically)
-                            .padding(start = 8.dp)
-                    ) { CenterArea(gases[randomAvatar]) }
+            else {
+                Surface (modifier = Modifier.fillMaxWidth(), color = Color.Transparent) {
                     Text(
                         text = "\n" + member.first + "\n\nStatus: " + member.second + "\n",
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(start = 8.dp).align(CenterVertically)
+                        modifier = Modifier.padding(start = 16.dp).align(CenterVertically)
                     )
                 }
             }
