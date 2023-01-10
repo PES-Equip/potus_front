e74abc03d6105e5d9b895a4cabfef258d61ce89e
BertaFito <berta.fito@estudiantat.upc.edu>
2023-01-10 23:04:56 +0100

    TG-254 #in-progress Message box, scrollable list and messaging screen have been implemented, although they do not technically work, yet

--- app/src/main/java/com/potus/potus_front/ui/screens/ChatScreen.kt
+++ app/src/main/java/com/potus/potus_front/ui/screens/ChatScreen.kt
@@ -1,5 +1,6 @@
 package com.potus.potus_front.ui.screens
 
+import android.view.WindowManager
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -11,6 +12,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -23,6 +25,7 @@ import com.potus.potus_front.composables.GardenBottomBar
 import com.potus.potus_front.composables.TopBar
 import com.potus.potus_front.google.models.TokenState
 import com.potus.potus_front.ui.theme.BraveGreen
+import com.potus.potus_front.ui.theme.Daffodil
 import com.potus.potus_front.ui.theme.SoothingGreen
 import com.potus.potus_front.websocket.StompMessage
 import com.potus.potus_front.websocket.StompMessageListener
@@ -33,10 +36,11 @@ import com.potus.potus_front.websocket.socketclient.ChatDeliver
 import com.potus.potus_front.websocket.socketclient.ChatListener
 import kotlinx.coroutines.Dispatchers
 import org.json.JSONObject
+import java.util.*
 
 
 @Composable
-fun ChatScreen(onNavigateToProfile: () -> Unit, onNavigateToShop: () -> Unit,  onNavigateToMeetings: () -> Unit, onNavigateToHome: () -> Unit, onNavigateToGarden: () -> Unit) {
+fun ChatScreen(onNavigateToProfile: () -> Unit, onNavigateToShop: () -> Unit,  onNavigateToChat: () -> Unit, onNavigateToMeetings: () -> Unit, onNavigateToHome: () -> Unit, onNavigateToGarden: () -> Unit) {
     val openDialog = remember { mutableStateOf(false)  }
     var actionString = remember { mutableStateOf("")  }
 
@@ -48,20 +52,31 @@ fun ChatScreen(onNavigateToProfile: () -> Unit, onNavigateToShop: () -> Unit,  o
     var chatListener: ChatListener = ChatListener(user.username)
     val topicHandler: TopicHandler = chatListener.subscribe("/chatroom/$room")
 
-    var message = remember { mutableStateOf("")  }
+    var message = remember { mutableStateOf("Please?")  }
 
     StompMessageSerializer.joinChat(user.username, room)
 
     chatListener.connect(StompMessageSerializer.url)
-    val chats = mutableMapOf<String,ChatMessage>()
+    val chats = mutableMapOf<String,ChatMessage>(
+        "0" to ChatMessage("Spartacus 0", "Hello!", "JOIN", "10/01/2023"),
+        "1" to ChatMessage("Spartacus 1", "Hello!", "JOIN", "10/01/2023"),
+        "2" to ChatMessage("Spartacus 2", "Hello!", "JOIN", "10/01/2023"),
+        "3" to ChatMessage("Spartacus 3", "Hello!", "JOIN", "10/01/2023"),
+        "4" to ChatMessage("Spartacus 4", "Hello!", "JOIN", "10/01/2023"),
+        "5" to ChatMessage("Spartacus 5", "Hello!", "JOIN", "10/01/2023"),
+        "6" to ChatMessage("Spartacus 6", "Hello!", "JOIN", "10/01/2023"),
+        "7" to ChatMessage("Spartacus 7", "Hello!", "JOIN", "10/01/2023"),
+        "8" to ChatMessage("Spartacus 8", "Hello!", "JOIN", "10/01/2023"),
+        "9" to ChatMessage("Spartacus 9", "Hello!", "JOIN", "10/01/2023"))
+
     val sml : StompMessageListener = object : StompMessageListener {
         override fun onMessage(message: StompMessage) {
 
             StompMessageSerializer.handleMessage(message,chats)
 
-            println("------")
-            println(chats)
-            println("------")
+            //println("------")
+            //println(chats)
+            //println("------")
             }
         }
     topicHandler.addListener(sml)
@@ -77,71 +92,82 @@ fun ChatScreen(onNavigateToProfile: () -> Unit, onNavigateToShop: () -> Unit,  o
             addedLeaves = 0,
             onNavigateToProfile = { onNavigateToProfile() }
         )
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            contentPadding = PaddingValues(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(chats.size) { arrayItem ->
-                Column(
+        Column (modifier = Modifier.weight(1f)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                OutlinedTextField(
+                    value = message.value,
+                    onValueChange = { message.value = it },
+                    label = { Text(text = "New message...") },
                     modifier = Modifier
-                        .fillMaxWidth()
+                        .weight(0.75f)
+                        .padding(horizontal = 8.dp),
+                    shape = MaterialTheme.shapes.medium
+                )
+                Button(
+                    onClick = {
+                        StompMessageSerializer.sendMessage(
+                            message = message.value,
+                            user.username,
+                            room
+                        )
+                        //chats += chats.size.toString() to ChatMessage("Them", "Hello!", "JOIN", "10/01/2023")
+                        //onNavigateToChat()
+                    },
+                    colors = ButtonDefaults.buttonColors(backgroundColor = BraveGreen),
+                    modifier = Modifier
+                        .weight(0.25f)
+                        .height(80.dp)
                         .padding(8.dp)
-                        .defaultMinSize(minHeight = 64.dp)
-                        .clip(RoundedCornerShape(10.dp))
-                        .background(SoothingGreen)
-                        .animateContentSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .align(Alignment.CenterVertically),
+                    shape = MaterialTheme.shapes.medium
                 ) {
-                    Row(modifier = Modifier.align(Alignment.Start)) {
-                        Image(
-                            painter = painterResource(id = com.potus.potus_front.R.drawable.icona_invitacio_tancada),
-                            "",
-                            modifier = Modifier
-                                .size(64.dp)
-                                .align(Alignment.CenterVertically)
-                                .padding(start = 8.dp)
-                        )
-                        Text(
-                            text = arrayItem.toString(),
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = BraveGreen,
-                            modifier = Modifier
-                                .padding(start = 8.dp)
-                                .align(Alignment.CenterVertically)
-                        )
+                    Text("SEND")
+                }
+            }
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                reverseLayout = true
+            ) {
+                items(chats.size) { chat ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                            .defaultMinSize(minHeight = 64.dp)
+                            .clip(RoundedCornerShape(10.dp))
+                            .background(Daffodil),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        val invertedPosition = chats.size - chat - 1
+                        Column (modifier = Modifier.fillMaxWidth().padding(8.dp)) {
+                            Text(
+                                text = chats[invertedPosition.toString()]?.sender.toString(),
+                                //text = chats[chat.toString()]?.sender.toString(),
+                                fontSize = 15.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Black,
+                                modifier = Modifier
+                                    .padding(start = 8.dp, bottom = 8.dp)
+                                    .align(Alignment.Start)
+                            )
+                            Text(
+                                text = chats[invertedPosition.toString()]?.message.toString(),
+                                //text = chats[chat.toString()]?.message.toString(),
+                                fontSize = 20.sp,
+                                color = BraveGreen,
+                                modifier = Modifier
+                                    .padding(start = 8.dp)
+                                    .align(Alignment.Start)
+                            )
+                        }
                     }
                 }
-            }}
-        Spacer(modifier = Modifier.weight(1f))
-        Row (
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-            OutlinedTextField(
-                value = message.value,
-                onValueChange = { message.value = it },
-                label = { Text(text = "New message...") },
-                modifier = Modifier
-                    .weight(0.8f)
-                    .padding(horizontal = 16.dp),
-                shape = MaterialTheme.shapes.medium
-            )
-            Button(
-                onClick = {
-                    StompMessageSerializer.sendMessage(message.value, user.username, room)
-                },
-                colors = ButtonDefaults.buttonColors(backgroundColor = BraveGreen),
-                modifier = Modifier
-                    .weight(0.3f)
-                    .height(80.dp)
-                    .padding(16.dp)
-                    .align(Alignment.CenterVertically),
-                shape = MaterialTheme.shapes.medium
-            ) {
-                Text("SEND")
             }
         }
         GardenBottomBar(painterResource(id = R.drawable.icona_meetings), onNavigateToMeetings, painterResource(id = R.drawable.basic), onNavigateToHome, painterResource(id = R.drawable.icona_jardi), onNavigateToGarden)
--- app/src/main/java/com/potus/potus_front/ui/screens/Navigation.kt
+++ app/src/main/java/com/potus/potus_front/ui/screens/Navigation.kt
@@ -126,6 +126,7 @@ fun Navigation(navController : NavHostController = rememberNavController()) {
             ChatScreen(
                 onNavigateToProfile = { navController.navigate(ProfileScreen.route) },
                 onNavigateToShop = { navController.navigate(GardenScreen.route) },
+                onNavigateToChat = { navController.navigate(ChatScreen.route) },
                 //TO BE IMPLEMENTED (CURRENTLY LEADING BACK TO GARDEN SCREEN)
                 onNavigateToMeetings = { navController.navigate(GardenScreen.route) },
                 onNavigateToHome = { navController.navigate(HomeScreen.route) },
