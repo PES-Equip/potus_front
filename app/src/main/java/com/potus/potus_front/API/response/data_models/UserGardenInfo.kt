759856f9ec4b312a448f7b0a315051f222e59933
BertaFito <berta.fito@estudiantat.upc.edu>
2023-01-11 13:36:30 +0100

    TG-256 #closed The Chat Screen works as intended

--- app/src/main/java/com/potus/potus_front/API/response/data_models/UserGardenInfo.kt
+++ app/src/main/java/com/potus/potus_front/API/response/data_models/UserGardenInfo.kt
@@ -1,8 +1,10 @@
 package com.potus.potus_front.API.response.data_models
 
 import com.potus.potus_front.API.response.NewGardenResponse
+import java.util.*
 
 data class UserGardenInfo(
+    var createdDate: Date,
     var garden: NewGardenResponse,
     var role: String
 )
